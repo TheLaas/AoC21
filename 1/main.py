e84f2aadadcc36be7659d9618fed036c1f5d2324
alexander laas <laas.alexander@gmail.com>
2021-12-05 21:14:51 +0100

    refactor day 1

--- 1/main.py
+++ 1/main.py
@@ -3,18 +3,20 @@ import numpy as np
 
 depth_data = np.array(reader.get_array("input.csv"))
 
-# Part 1
-difference_to_prev = depth_data[1:] - depth_data[:-1]
-increased_from_prev = difference_to_prev > 0
 
-number_of_increased_from_prev = sum(increased_from_prev)
-print(f'Part1: {number_of_increased_from_prev}')
+def change_to_prev(array: np.ndarray) -> np.ndarray:
+    return array[1:] - array[:-1]
+
+
+def number_of_increased_from_prev(array: np.ndarray) -> int:
+    difference_to_prev = change_to_prev(array)
+    return sum(difference_to_prev > 0)
+
+
+# Part 1
+print(f'Part1: {number_of_increased_from_prev(depth_data)}')
 
 # Part 2
 aggregated_depth = depth_data[:-2] + depth_data[1:-1] + depth_data[2: ]
-difference_to_prev = aggregated_depth[1:] - aggregated_depth[:-1]
-increased_from_prev = difference_to_prev > 0
-
-number_of_increased_from_prev = sum(increased_from_prev)
-print(f'Part2: {number_of_increased_from_prev}')
+print(f'Part2: {number_of_increased_from_prev(aggregated_depth)}')
 

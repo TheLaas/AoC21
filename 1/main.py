83ad4ebbc5b49a00745ea0d2398ae8cefad3be1e
alexander laas <laas.alexander@gmail.com>
2021-12-05 21:11:42 +0100

    day 1 done

--- 1/main.py
+++ 1/main.py
@@ -2,8 +2,19 @@ from common import reader
 import numpy as np
 
 depth_data = np.array(reader.get_array("input.csv"))
+
+# Part 1
 difference_to_prev = depth_data[1:] - depth_data[:-1]
 increased_from_prev = difference_to_prev > 0
 
 number_of_increased_from_prev = sum(increased_from_prev)
-print(number_of_increased_from_prev)
+print(f'Part1: {number_of_increased_from_prev}')
+
+# Part 2
+aggregated_depth = depth_data[:-2] + depth_data[1:-1] + depth_data[2: ]
+difference_to_prev = aggregated_depth[1:] - aggregated_depth[:-1]
+increased_from_prev = difference_to_prev > 0
+
+number_of_increased_from_prev = sum(increased_from_prev)
+print(f'Part2: {number_of_increased_from_prev}')
+

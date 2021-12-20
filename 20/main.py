287d0afe32a73faf1bac96f22f7620586e7b2f15
alexander laas <laas.alexander@gmail.com>
2021-12-20 21:32:21 +0100

    day 20 pseudo

---
+++ 20/main.py
@@ -0,0 +1,27 @@
+from common import reader
+from typing import List, Tuple
+
+lines = reader.get_lines("test.txt")
+
+def separate_input(lines: List[str]) -> Tuple[str, List[str]]:
+    pass
+
+
+def traslate_pixel(region: List[str], padding_symbol: str) -> str:
+    pass
+
+
+def iterate_image(input_image: List[str]) -> List[str]:
+    pass
+
+
+def calc_active_pixels(image: List[str]) -> int:
+    pass
+
+def get_9_grid_indices(x_ind: int, y_ind_int) -> List[str]:
+    pass
+
+enhance_algo, input_image = separate_input(lines)
+
+
+# Part 1
